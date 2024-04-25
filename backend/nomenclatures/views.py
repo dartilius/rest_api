e5cead233e17a1bc190ffb04dd493387556f23bd
Шалейников Евгений <shaleinikove@krasrm.com>
2024-04-25 17:42:36 +0700

    правки по свежим изменениям

--- backend/nomenclatures/views.py
+++ backend/nomenclatures/views.py
@@ -28,27 +28,7 @@ class NomenclatureViewSet(viewsets.ModelViewSet):
     # permission_classes = [AuthAndOnlySuperUserDelete, ]
 
     def get_serializer(self, *args, **kwargs):
-        if 'data' in kwargs:
-            data = kwargs['data']
-
-            if isinstance(data, list):
-                kwargs['many'] = True
-
-        return super().get_serializer(*args, **kwargs)
-
-    def perform_create(self, serializer):
-        nomenclature = serializer.save(owner=User.objects.get(pk=1))
-        group = NomenclatureGroup.objects.create(
-            owner=self.request.user,
-            name=nomenclature.name,
-            description=nomenclature.name,
-            settings=settings
-        )
-        group.clients.add(nomenclature)
-        group.save()
-
-    def get_serializer(self, *args, **kwargs):
-        if self.request.method == 'GET' and not kwargs['detail']:
+        if self.request.method == 'GET' and not kwargs.get('detail'):
             serializer = NomenclatureListSerializer
         else:
             serializer = NomenclatureSerializer
@@ -59,6 +39,17 @@ class NomenclatureViewSet(viewsets.ModelViewSet):
                 kwargs['many'] = True
 
         return serializer(*args, **kwargs)
+
+    def perform_create(self, serializer):
+        nomenclature = serializer.save(owner=User.objects.get(pk=1))
+        group = NomenclatureGroup.objects.create(
+            owner=self.request.user,
+            name=nomenclature.name,
+            description=nomenclature.name,
+            settings=serializer.data['settings']
+        )
+        group.clients.add(nomenclature)
+        group.save()
 
 
 class HardWareInfoViewSet(viewsets.ModelViewSet):
