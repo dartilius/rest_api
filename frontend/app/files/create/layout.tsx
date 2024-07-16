829539e085037ea7660fb09da021de441caa3b50
Pablo <pahapasha11@gmail.com>
2024-07-16 17:30:40 +0700

    wip: что-то с конфигами сделал и удаление

--- frontend/app/files/create/layout.tsx
+++
@@ -1,9 +0,0 @@
-import { Metadata } from "next";
-
-import FilesCreate from "./page";
-
-export const metadata: Metadata = {
-  title: "Создание файла",
-  description: "Создание файла и тэгов",
-};
-export default FilesCreate;
--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,10 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   // output: 'export',
-  trailingSlash: true,
-  // typescript: {
-  //   ignoreBuildErrors: true,
-  // },
+  // trailingSlash: true,
+  typescript: {
+    ignoreBuildErrors: true,
+  },
   output: "standalone",
   redirects() {
     return [
