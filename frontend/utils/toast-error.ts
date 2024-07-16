364b0a551b2ec7195c3eed09169ddd502c029323
Pablo <pahapasha11@gmail.com>
2024-07-16 17:31:08 +0700

    wip: удаление 2

--- frontend/utils/toast-error.ts
+++
@@ -1,9 +0,0 @@
-import { toastr } from "react-redux-toastr";
-
-import { errorCatch } from "../api/api.helpers";
-
-export const toastError = (error: any, title?: string) => {
-  const message = errorCatch(error);
-
-  toastr.error(title || "Ошибка", message);
-};
