4291799a9c52afc01b1958f15245586c6eb51410
Borisov Ilya <il.borisoff2010@yandex.ru>
2024-09-04 10:18:07 +0700

    lj,fdbk обновление статусов доступности и запиÑсь истории иее изменения чеÐрез цлÐелери

--- backend/rmc_rest_api/celery.py
+++ backend/rmc_rest_api/celery.py
@@ -1,7 +1,20 @@
 import os
 from celery import Celery
+from celery.schedules import crontab
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
-app = Celery('backend')
-app.config_from_object('django.conf:settings', namespace='CELERY')
+
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", 'rmc_rest_api.settings')
+
+app = Celery('rmc_rest_api')
+app.config_from_object('django.conf:settings', namespace="CELERY")
 app.autodiscover_tasks()
+
+
+app.conf.beat_schedule = {
+    'every': {
+        'task': 'nomenclatures.tasks.update_nomenclature_status',
+        'schedule': crontab(),
+    },
+
+}
