1452ea6dfcdb3be1e06a83127839b607df9b3a76
Ilya Borisov <borisovi@krasrm.com>
2024-09-06 17:36:03 +0700

    celery task debug

--- backend/rmc_rest_api/celery.py
+++ backend/rmc_rest_api/celery.py
@@ -1,20 +1,21 @@
 import os
-from celery import Celery
-from celery.schedules import crontab
 
+from rmc_rest_api import settings
+from celery import Celery
 
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", 'rmc_rest_api.settings')
 
 app = Celery('rmc_rest_api')
 app.config_from_object('django.conf:settings', namespace="CELERY")
+app.conf.singleton_backend_url = settings.CELERY_SINGLETON_BACKEND_URL
 app.autodiscover_tasks()
 
 
 app.conf.beat_schedule = {
     'every': {
         'task': 'nomenclatures.tasks.update_nomenclature_status',
-        'schedule': crontab(),
+        'schedule': 5.0,
     },
 
 }
