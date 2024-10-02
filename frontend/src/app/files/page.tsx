0c6e2e27814344fafe04dda8f092b74625ca6d0d
Шалейников Евгений <shaleinikove@krasrm.com>
2024-10-02 05:47:42 +0000

    Update page.tsx

--- frontend/src/app/files/page.tsx
+++ frontend/src/app/files/page.tsx
@@ -1,29 +1,29 @@
 import FilesListClientPage from "./FilesListClientPage";
 
-import filesService from "@/src/services/files/files.service";
+// import filesService from "@/src/services/files/files.service";
 
-export async function generateMetadata() {
-  const page = 1;
-  const limit = 10;
+// export async function generateMetadata() {
+//   const page = 1;
+//   const limit = 10;
 
-  try {
-    const response = await filesService.getAll({ page, limit });
+//   try {
+//     const response = await filesService.getAll({ page, limit });
 
-    if (response) {
-      return {
-        title: `Файлы ${response.data.count} штук(-и)`,
-        description: `Просмотр списка файлов ${response.data.count} штук(-и)`,
-      };
-    }
-  } catch (error) {
-    console.error("Failed to fetch metadata:", error);
-  }
+//     if (response) {
+//       return {
+//         title: `Файлы ${response.data.count} штук(-и)`,
+//         description: `Просмотр списка файлов ${response.data.count} штук(-и)`,
+//       };
+//     }
+//   } catch (error) {
+//     console.error("Failed to fetch metadata:", error);
+//   }
 
-  return {
-    title: "Default List Title",
-    description: "Default List Description",
-  };
-}
+//   return {
+//     title: "Default List Title",
+//     description: "Default List Description",
+//   };
+// }
 
 export default async function ListPage() {
   return <FilesListClientPage />;
