ec06e8a8e778f9863a7344af58d87358b100f851
Pavel <pahapasha11@gmail.com>
2024-10-02 12:14:35 +0700

    еуые

--- frontend/src/app/nomenclatures/page.tsx
+++ frontend/src/app/nomenclatures/page.tsx
@@ -1,8 +1,30 @@
-import { generateMetadata } from "./metadata";
-import ListPage from "./ListPage";
+"use client";
 
-export { generateMetadata };
+import NomenclaturesList from "./NomenclaturesListClientPage";
+
+import Loader from "@/src/components/ui/Loader";
+import { toastError } from "@/src/utils/toast-error";
+import useNomenclaturesQuery from "@/src/hooks/nomenclatures/useNomenclaturesQuery";
 
 export default function Page() {
-  return <ListPage />;
+  const page = 1;
+  const limit = 10;
+  const { isLoading, error, isError, isSuccess } = useNomenclaturesQuery({
+    page,
+    limit,
+  });
+
+  if (isLoading) {
+    return <Loader loading={!isSuccess} />;
+  }
+
+  if (isError) {
+    return <>{toastError(error?.message)}</>;
+  }
+
+  if (isSuccess) {
+    return <NomenclaturesList />;
+  }
+
+  return <></>;
 }
