eade8798b97e761eeb81f99767cc869e00e0731a
Pavel <pahapasha11@gmail.com>
2024-10-02 16:48:03 +0700

    feat music player

--- frontend/src/app/playlists/[id]/page.tsx
+++ frontend/src/app/playlists/[id]/page.tsx
@@ -2,14 +2,14 @@
 import useIdFromParams from "@/src/hooks/useIdFromParams";
 import { usePlaylistQuery } from "@/src/hooks/playlists/usePlaylistQuery";
 import { useEffect, useRef, useState } from "react";
-import {Button} from "@nextui-org/react";
+import {Button, Skeleton} from "@nextui-org/react";
 
 function PlaylistPage() {
     const id = useIdFromParams();
     const audioRef = useRef<HTMLAudioElement>(null);
     const [currentTrack, setCurrentTrack] = useState<number>(0);
-    const [pause, setPause] = useState<'pause' | 'play'>('pause')
     const { data, isLoading } = usePlaylistQuery(id);
+    const [isAutoPlay, setIsAutoPlay] = useState<boolean>(false);
     const [isPlaying, setIsPlaying] = useState<boolean>(false);
 
     // Чтобы хуки всегда вызывались одинаково, используем заглушку для треков
@@ -18,12 +18,14 @@ function PlaylistPage() {
     const playNext = () => {
         if (tracks.length > 0) {
             setCurrentTrack((prevIndex) => (prevIndex + 1) % tracks.length);
+            setIsAutoPlay(true);
         }
     };
 
     const playPrev = () => {
         if (tracks.length > 0) {
             setCurrentTrack((prevIndex) => (prevIndex - 1 + tracks.length) % tracks.length);
+            setIsAutoPlay(true);
         }
     }
 
@@ -39,35 +41,45 @@ function PlaylistPage() {
     };
 
     useEffect(() => {
-        if (audioRef.current && tracks.length > 0) {
+        // Воспроизводим только если трек переключился автоматически
+        if (audioRef.current && isAutoPlay) {
             audioRef.current.play();
+            setIsPlaying(true); // Обновляем состояние воспроизведения
+            setIsAutoPlay(false); // Сбрасываем флаг автопроигрывания
         }
-    }, [currentTrack, tracks]);
-
-    // Можно показывать индикатор загрузки, пока данные загружаются
-    if (isLoading) {
-        return <div>Loading...</div>;
-    }
-
-    if (tracks.length === 0) {
-        return <div>No tracks available</div>;
-    }
+    }, [currentTrack, isAutoPlay]);
 
     return (
         <div>
-            <div className='flex flex-row items-center justify-center gap-2'>
-                <Button onClick={playPrev}>Prev</Button>
-                <audio
-                    ref={audioRef}
-                    src={tracks[currentTrack]?.url}
-                    controls
-                    onEnded={playNext}
-                    autoPlay={false}
-                />
-                <Button onClick={togglePlayPause}>{isPlaying ? "Pause" : "Play"}</Button>
-                <Button onClick={playNext}>Next</Button>
+            {isLoading ?
+                <Skeleton className="w-3/5 rounded-lg">
+                    <div className="h-3 w-3/5 rounded-lg bg-default-200"></div>
+                </Skeleton>
+                :
+                <div>
+                    <div className='flex flex-row items-center justify-center gap-2'>
+                        <Button onClick={playPrev}>Prev</Button>
+                        <audio
+                            ref={audioRef}
+                            src={tracks[currentTrack]?.url}
+                            controls
+                            onEnded={playNext}
+                            autoPlay={false}
+                        />
+                        <Button onClick={togglePlayPause}>{isPlaying ? "Pause" : "Play"}</Button>
+                        <Button onClick={playNext}>Next</Button>
+                    </div>
+                    <p>Сейчас играет: {tracks[currentTrack]?.name}</p>
+                </div>
+            }
+            <div className='flex flex-row items-center gap-2'>
+                <h2>Название плейлиста</h2>
+                <span>{data?.name}</span>
             </div>
-            <p>Сейчас играет: {tracks[currentTrack]?.name}</p>
+            <div className='flex flex-row items-center gap-2'>
+                <h2>Описание</h2>
+                    <span>{data?.description}</span>
+            </div>
         </div>
     );
 }
