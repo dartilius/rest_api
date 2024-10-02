cd5623e5b776a718ad490a9067d8a7dac087113e
Pavel <pahapasha11@gmail.com>
2024-10-02 11:21:00 +0700

    fix

--- frontend/src/app/files/[id]/page.tsx
+++ frontend/src/app/files/[id]/page.tsx
@@ -84,16 +84,7 @@ export default function ReadFile() {
         <Divider />
         <CardFooter className="flex justify-center flex-col gap-2">
           <p className="text-md">Заглушка</p>
-          <audio autoPlay={true} controls src="http://192.168.0.180:9000/local-media/music/%D0%9F%D0%B0%D1%88%D0%B0_%D0%A2%D0%B5%D1%85%D0%BD%D0%B8%D0%BA_%D0%9D%D1%83%D0%B6%D0%B5%D0%BD_%D0%9A%D1%81%D0%B0%D0%BD%D0%B0%D0%BA%D1%81_%D0%BD%D0%BE_%D1%8D%D1%82%D0%BE_%D0%B3%D1%80%D0%B8%D0%B3%D0%BE%D1%80%D0%B8%D0%B0%D0%BD%D1%81%D0%BA%D0%B8%D0%B9_%D1%85%D0%BE%D1%80.mp3?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=jV1io3iRsfHapjDWLZV2%2F20240930%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20240930T075704Z&X-Amz-Expires=604800&X-Amz-SignedHeaders=host&X-Amz-Signature=8d074f618de7a0909b3ef6c82b32bb071f1e2d49dfcc022a1d8dcf2c8b1e2478"></audio>
-
-          {/*<Image*/}
-          {/*  alt={`${data?.name}`}*/}
-          {/*  height={240}*/}
-          {/*  loading="lazy"*/}
-          {/*  radius="sm"*/}
-          {/*  src="https://bigpicture.ru/wp-content/uploads/2014/12/luchshie-foto-nedeli-v-dek-2014-0.jpg"*/}
-          {/*  width={240}*/}
-          {/*/>*/}
+          <p>не робит</p>
         </CardFooter>
       </Card>
     </div>
