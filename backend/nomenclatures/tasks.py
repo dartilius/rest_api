45cbfe4cc11104fd10477193641d90006487bdd4
Ilya Borisov <borisovi@krasrm.com>
2024-10-21 12:27:13 +0700

    bugfix

--- backend/nomenclatures/tasks.py
+++ backend/nomenclatures/tasks.py
@@ -12,9 +12,6 @@ def update_nomenclature_status():
     и запись истории их изменения.
     """
     statuses = NomenclatureAvailability.objects.all()
-    count_updated_statuses = 0
-    count_to_update_and_create = 0
-    update_and_create_threshold = 10
     statuses_to_update = []
     status_histories_to_create = []
 
@@ -37,8 +34,6 @@ def update_nomenclature_status():
                         client=status.client
                     )
                 )
-                count_updated_statuses += 1
-                count_to_update_and_create += 1
 
         if current_status == 1:
             new_status = 1
@@ -55,8 +50,6 @@ def update_nomenclature_status():
                         client=status.client
                     )
                 )
-                count_updated_statuses += 1
-                count_to_update_and_create += 1
 
         if current_status == 2:
             if now_time - last_answer < timedelta(minutes=5):
@@ -68,12 +61,8 @@ def update_nomenclature_status():
                         client=status.client
                     )
                 )
-                count_updated_statuses += 1
-                count_to_update_and_create += 1
 
-        if count_to_update_and_create >= update_and_create_threshold:
-            NomenclatureAvailability.objects.bulk_update(statuses_to_update)
-            StatusHistory.objects.bulk_create(status_histories_to_create)
-            count_to_update_and_create = 0
+        NomenclatureAvailability.objects.bulk_update(statuses_to_update, ["status"])
+        StatusHistory.objects.bulk_create(status_histories_to_create)
 
-    return f"Обновлено {count_updated_statuses} статусов доступности."
+    return f"Обновлено {len(statuses_to_update)} статусов доступности."
