11dae33ddbda6583869a8a284cf3c040096c9d9a
Шалейников Евгений <shaleinikove@krasrm.com>
2025-01-31 12:31:57 +0700

    добавил экшн для удаления токенов в админ-панели

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -1,10 +1,13 @@
-from django.contrib import admin
+from django.contrib import admin, messages
+from django.utils.translation import ngettext
 from rest_framework_simplejwt.token_blacklist.admin import OutstandingTokenAdmin
 from rest_framework_simplejwt.token_blacklist.models import OutstandingToken
 
 
 class CustomOutstandingTokenAdmin(OutstandingTokenAdmin):
     """Даёт возможность удалять токены через админ-панель."""
+
+    actions = ['delete']
 
     def has_delete_permission(self, *args, **kwargs):
         return True
@@ -14,6 +17,23 @@ class CustomOutstandingTokenAdmin(OutstandingTokenAdmin):
         if 'delete_selected' in actions:
             del actions['delete_selected']
         return actions
+
+    @admin.action(description='Удалить выбранные токены')
+    def delete(self, request, queryset):
+        """Удаление токенов."""
+        try:
+            updated = queryset.delete()
+            self.message_user(
+                request,
+                ngettext(
+                    f'{updated} запрос на отмену заказа принят',
+                    f'{updated} запросов на отмену заказов принято',
+                    updated,
+                ),
+                messages.SUCCESS
+            )
+        except Exception as e:
+            self.message_user(request, e, messages.ERROR)
 
 
 admin.site.unregister(OutstandingToken)
