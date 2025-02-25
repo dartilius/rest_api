a0970f2308ca8c8c6b45d38fabbc4a6255f286f7
Aleksandr Vladyko <mett0@bk.ru>
2025-02-25 22:32:08 +0700

    fix: styles

--- frontend/src/components/tabs/TabsPanel.tsx
+++ frontend/src/components/tabs/TabsPanel.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import { useStore } from '@/providers/mobx-provider/MobxProvider'
-import { AppBar, Box, Paper, Tab, Tabs } from '@mui/material'
+import { AppBar, Paper, Tab, Tabs } from '@mui/material'
 import { observer } from 'mobx-react'
 import BgOrders from '../bg-orders/BgOrders'
 import AdOrders from '../ad-orders/AdOrders'
@@ -14,28 +14,25 @@ const TabsPanel = () => {
   }
   const activeTab = ordersStore.activeTab
   return (
-    <Box sx={{ width: '100%', height: '100%' }}>
+    <Paper elevation={4} sx={{ width: '100%', height: '100%' }}>
+      <AppBar position='static' color='transparent'>
+        <Tabs
+          value={activeTab}
+          onChange={handleChange}
+          indicatorColor='primary'
+          textColor='inherit'
+          variant='fullWidth'
+          aria-label='full width tabs example'
+        >
+          <Tab color='secondary' value={0} label='Bg-Orders' />
+          <Tab value={1} label='AD-Orders' />
+        </Tabs>
+      </AppBar>
 
-      <Paper elevation={4} sx={{height: '100%'}}>
-        <AppBar position='static' color='transparent'>
-          <Tabs
-            value={activeTab}
-            onChange={handleChange}
-            indicatorColor='primary'
-            textColor='inherit'
-            variant='fullWidth'
-            aria-label='full width tabs example'
-          >
-            <Tab color='secondary' value={0} label='Bg-Orders' />
-            <Tab value={1} label='AD-Orders' />
-          </Tabs>
-        </AppBar>
+      {activeTab === 0 && <BgOrders />}
 
-        {activeTab === 0 && <BgOrders />}
-
-        {activeTab === 1 && <AdOrders />}
-      </Paper>
-    </Box>
+      {activeTab === 1 && <AdOrders />}
+    </Paper>
   )
 }
 export default observer(TabsPanel)
