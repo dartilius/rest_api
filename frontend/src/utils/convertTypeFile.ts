82ba99483b0eb28bbf540ca5c9f63cb17d8649e4
pablito <pahapasha11@gmail.com>
2025-03-31 11:21:41 +0700

    feat  - convert size file  - guess type file

--- frontend/src/utils/convertTypeFile.ts
+++ frontend/src/utils/convertTypeFile.ts
@@ -23,3 +23,26 @@ type TypeKey = keyof typeof typesForSend;
 export const convertTypeForSendFile = (key: string): number | string => {
     return typesForSend[key as TypeKey] ?? 'Неизвестный тип';
 };
+
+export const guessType = (fileName: string) => {
+    const ext = fileName.split(".").pop()
+
+    const imageExtensions = [
+        "png",
+        "gif",
+        "jpg",
+        "ico",
+        "jfif",
+        "jpeg",
+        "bmp",
+        "tif",
+        "tiff",
+    ];
+    const audioExtensions = ["mp3", "wav"];
+    const videoExtensions = ["mp4", "mov", "avi"];
+
+    if (ext && imageExtensions.includes(ext.toLowerCase())) return "image";
+    if (ext && ["svg"].includes(ext.toLowerCase())) return "svg";
+    if (ext && audioExtensions.includes(ext.toLowerCase())) return "audio";
+    if (ext && videoExtensions.includes(ext.toLowerCase())) return "video";
+}
