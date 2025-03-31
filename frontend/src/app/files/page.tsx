5a0d985ca7a17e200385846ab4f78d863f6bd994
pablito <pahapasha11@gmail.com>
2025-03-31 11:24:08 +0700

    feat

--- frontend/src/app/files/page.tsx
+++ frontend/src/app/files/page.tsx
@@ -1,13 +1,12 @@
-import {Metadata} from "next";
-import {List} from "@/app/files/componetns/List";
-import {fetchFilesList} from "@/services/FilesService";
-import {ModalWrapper} from "@/app/files/componetns/Modal";
-import SearchForm from "@/app/files/componetns/SearchForm";
+import { Metadata } from "next";
+import { fetchFilesList } from "@/services/FilesService";
+import Card from "@/app/files/components/Card/Card";
+import { Pagination } from "@/app/files/components/Pagination";
 
 export const metadata: Metadata = {
-        title: 'Файлы',
-        description: 'Список файлов'
-}
+    title: "Файлы",
+    description: "Список файлов",
+};
 
 type FilesListProps = {
     page?: number | string;
@@ -15,27 +14,27 @@ type FilesListProps = {
     name?: string;
     file_type?: string;
     tags?: string;
-}
+};
 
-export default async function Page(props: {
-    searchParams?: Promise<FilesListProps>
-}) {
-    const searchParams = await props.searchParams
-    const name = searchParams?.name || '';
+export default async function Page(props: { searchParams?: Promise<FilesListProps> }) {
+    const searchParams = await props.searchParams;
+    const name = searchParams?.name || "";
     const currentPage = Number(searchParams?.page) || 1;
-    const limit = Number(searchParams?.limit) || 10
-    const fileType = searchParams?.file_type || '';
-    const tags = searchParams?.tags || '';
+    const limit = Number(searchParams?.limit) || 12;
+    const fileType = searchParams?.file_type || "";
+    const tags = searchParams?.tags || "";
 
-    const listFiles = await fetchFilesList({page: currentPage,limit, name})
-    const dataFiles =  typeof listFiles !== 'string' ? listFiles.results : []
-    const countFiles =  typeof listFiles !== 'string' ? listFiles.count : 0
+    const listFiles = await fetchFilesList({ page: currentPage, limit, name });
+    const dataFiles = typeof listFiles !== "string" ? listFiles.results : [];
+    const countFiles = typeof listFiles !== "string" ? listFiles.count : 0;
+    console.log(dataFiles)
+
     return (
-        <div>
-            <ModalWrapper />
-            <SearchForm />
-
-            <List limit={limit} currentPage={currentPage} data={dataFiles} count={countFiles}/>
-        </div>
+        <>
+            <div>
+                <Card data={dataFiles}/>
+            </div>
+            <Pagination currentPage={currentPage} totalPages={Math.ceil(countFiles / limit)} />
+        </>
     );
 }
