90a67bbabf30632f6de3f33097da2a1a4b358dfb
pablito <pahapasha11@gmail.com>
2025-04-22 16:59:58 +0700

    fef

--- frontend/src/app/nomenclatures/components/Table/TableNomenclatures.tsx
+++ frontend/src/app/nomenclatures/components/Table/TableNomenclatures.tsx
@@ -36,7 +36,7 @@ export function TableNomenclatures(props: Props) {
 			className={styles.custom_scroll}
 			sx={{
 				maxWidth: '100%',
-				maxHeight: '870px',
+				maxHeight: '874px',
 				height: '100%',
 				borderRadius: '8px',
 				backgroundColor: 'white',
