c9439e93e2f91655ee05637397208060a301af54
Borisov Ilya <il.borisoff2010@yandex.ru>
2025-04-28 22:52:29 +0700

    Убрал next и previos из респонса как просил Паша

--- backend/api/pagination.py
+++ backend/api/pagination.py
@@ -9,8 +9,6 @@ class PageLimitPagination(PageNumberPagination):
     def get_paginated_response(self, data, **kwargs):
         return Response({
             'count': self.page.paginator.count,
-            'next': self.get_next_link(),
-            'previous': self.get_previous_link(),
             'results': data,
             **kwargs,
         })
