6d3f410fc0c8e4bde8c6a88a57f93f024e209d21
Borisov Ilya <il.borisoff2010@yandex.ru>
2025-05-12 22:37:27 +0700

    Вернул поля в респонс пагинации, позже переделаю методы для получения следующей и предыдущей страницы (чтобы возвращались только номера страниц)

--- backend/api/pagination.py
+++ backend/api/pagination.py
@@ -9,6 +9,8 @@ class PageLimitPagination(PageNumberPagination):
     def get_paginated_response(self, data, **kwargs):
         return Response({
             'count': self.page.paginator.count,
+            'next': self.get_next_link(),
+            'previous': self.get_previous_link(),
             'results': data,
             **kwargs,
         })
