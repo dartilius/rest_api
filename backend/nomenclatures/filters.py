cbcbbf7c9d9d818d4a5fa81a786b70d50981740a
zabor <pahapasha11@gmail.com>
2025-10-23 11:19:15 +0700

    feat(nomenclature/filter): добавлены фильтры и сортировки в списке

--- backend/nomenclatures/filters.py
+++ backend/nomenclatures/filters.py
@@ -1,8 +1,15 @@
-from django_filters import AllValuesMultipleFilter, CharFilter, FilterSet
+from django_filters import AllValuesMultipleFilter, CharFilter, FilterSet, UUIDFilter, BaseInFilter, OrderingFilter
 
 from nomenclatures.models import Nomenclature
 
 
+class UUIDCommaInFilter(BaseInFilter, UUIDFilter):
+    """Поддерживает фильтрацию UUID через запятую (в URL)."""
+    def filter(self, qs, value):
+        if value:
+            if isinstance(value, str):
+                value = value.split(",")
+        return super().filter(qs, value)
 class NomenclatureFilter(FilterSet):
     """
     Фильтрация номенклатур.
@@ -14,6 +21,8 @@ class NomenclatureFilter(FilterSet):
         name        - частичное совпадение
         id          - точное совпадение
         timezone    - точное совпадение
+        brand_id    - совпадение по множеству id брендов через ',' без пробела
+        code1c      - точное совпадение
     """
 
     versions = AllValuesMultipleFilter(field_name='version')
@@ -22,12 +31,33 @@ class NomenclatureFilter(FilterSet):
     name = CharFilter(field_name='name', lookup_expr='icontains')
     id = CharFilter(field_name='id', lookup_expr='iexact')
     timezone = CharFilter(field_name='timezone', lookup_expr='iexact')
-    brand_name = CharFilter(field_name='brand__name', lookup_expr='icontains')
+    brand_id = UUIDCommaInFilter(field_name='brand_id', lookup_expr='in')
     code1c = CharFilter(field_name="code1c", lookup_expr="icontains")
+
+    ordering = OrderingFilter(
+        fields=(
+            ('name', 'name'),
+            ('version', 'version'),
+            ('timezone', 'timezone'),
+            ('pricePerMonth', 'pricePerMonth'),
+            ('created', 'created'),
+            ('brand__name', 'brand_name'),
+        ),
+        field_labels={
+            'name': 'Название',
+            'version': 'Версия ПО',
+            'timezone': 'Часовой пояс',
+            'code1c': 'Код 1С',
+            'pricePerMonth': 'Цена за месяц',
+            'created': 'Дата создания',
+            'brand__name': 'Название бренда',
+            'brand__code1c': 'Код 1С бренда',
+        }
+    )
 
     class Meta:
         model = Nomenclature
-        fields = ('name', 'id', 'timezone', 'versions', 'status', 'brand_name', "code1c")
+        fields = ('name', 'id', 'timezone', 'versions', 'status', 'brand_id', "code1c")
 
     def get_status(self, queryset, name, value):
         """
