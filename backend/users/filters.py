1a429a5ab70d257aaee65620ffcd406b1b79bcde
zabor <pahapasha11@gmail.com>
2025-12-18 11:42:11 +0700

    fix

--- backend/users/filters.py
+++ backend/users/filters.py
@@ -24,7 +24,7 @@ class CustomUserFilter(FilterSet):
         model = CustomUser
         fields = ('id', 'role', 'created', 'name')
 
-    def filter_by_owner_name(queryset, name, value):
+    def filter_by_name(self, queryset, name, value):
         return queryset.filter(
             Q(first_name__icontains=value) |
             Q(last_name__icontains=value) |
