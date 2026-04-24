7679a8d240cc329ecf63196add06902c9fe28c60
zabor <pahapasha11@gmail.com>
2026-04-24 13:57:21 +0700

    feat

--- backend/nomenclatures/serializers.py
+++ backend/nomenclatures/serializers.py
@@ -1278,7 +1278,7 @@ class NomenclatureCardSerializer(serializers.ModelSerializer):
     formattedAddress = serializers.SerializerMethodField()
     typeOfPlace = serializers.CharField(source="type_of_place_display", read_only=True)
     nameForFront = serializers.CharField(source="name_for_front", read_only=True)
-    slotsPerHour = serializers.SerializerMethodField(source="slots_per_hour", read_only=True)
+    slots_per_hour = serializers.SerializerMethodField()
 
     class Meta:
         model = Nomenclature
@@ -1290,7 +1290,7 @@ class NomenclatureCardSerializer(serializers.ModelSerializer):
             "formattedAddress",
             "typeOfPlace",
             "pricePerMonth",
-            "slotsPerHour"
+            "slots_per_hour"
         )
         read_only_fields = fields
 
