a85a50d7a519d97e0ef4164e051b426c559d2e32
zabor <pahapasha11@gmail.com>
2026-04-27 14:25:28 +0700

    fix

--- backend/rmc_rest_api/settings.py
+++ backend/rmc_rest_api/settings.py
@@ -55,7 +55,6 @@ DEFAULT_FROM_EMAIL = os.environ.get('DEFAULT_FROM_EMAIL')
 EMAIL_HOST = os.environ.get('EMAIL_HOST')
 EMAIL_PORT = int(os.environ.get('EMAIL_PORT', 587))
 EMAIL_USE_TLS = os.environ.get('EMAIL_USE_TLS', 'true').lower() == 'true'
-EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
 
 
