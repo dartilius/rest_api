468f95784b4c617735143e81f0dcdb8a85ade524
zabor <pahapasha11@gmail.com>
2026-04-27 16:28:02 +0700

    fix

--- backend/rmc_rest_api/settings.py
+++ backend/rmc_rest_api/settings.py
@@ -54,12 +54,12 @@ INSTALLED_APPS = [
 import ssl
 DEFAULT_FROM_EMAIL = os.environ.get('DEFAULT_FROM_EMAIL')
 EMAIL_HOST = os.environ.get('EMAIL_HOST')
-EMAIL_PORT = int(os.environ.get('EMAIL_PORT', 587))
 EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER', '')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
+EMAIL_PORT = int(os.environ.get('EMAIL_PORT', 587))
 
 # Настройки TLS/SSL
-EMAIL_USE_TLS = os.environ.get('EMAIL_USE_TLS', 'true').lower() == 'true'
+EMAIL_USE_TLS = os.environ.get('EMAIL_USE_TLS', 'True') == 'True'
 EMAIL_USE_SSL = os.environ.get('EMAIL_USE_SSL', 'false').lower() == 'true'
 
 # Выбор бэкенда
