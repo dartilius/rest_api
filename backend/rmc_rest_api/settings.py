062215f40e7b2eebcdf9fcab7894e27ff2720483
zabor <pahapasha11@gmail.com>
2026-04-27 15:13:44 +0700

    fix

--- backend/rmc_rest_api/settings.py
+++ backend/rmc_rest_api/settings.py
@@ -53,6 +53,7 @@ INSTALLED_APPS = [
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 DEFAULT_FROM_EMAIL = os.environ.get('DEFAULT_FROM_EMAIL')
 EMAIL_HOST = os.environ.get('EMAIL_HOST')
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
 EMAIL_PORT = int(os.environ.get('EMAIL_PORT', 587))
 EMAIL_USE_TLS = os.environ.get('EMAIL_USE_TLS', 'true').lower() == 'true'
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
