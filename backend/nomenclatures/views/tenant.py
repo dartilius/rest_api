02b0695ea3e35e4824d21811631895fc9b269c2d
zabor <pahapasha11@gmail.com>
2026-04-29 16:24:46 +0700

    chec

--- backend/nomenclatures/views/tenant.py
+++ backend/nomenclatures/views/tenant.py
@@ -39,6 +39,7 @@ class NomenclatureTenantViewSet(viewsets.ModelViewSet):
     filter_backends = [DjangoFilterBackend, SearchFilter]
     filterset_class = NomenclatureTenantFilter
     # pagination_class = CustomLimitOffsetPagination
+    pagination_class = None
     search_fields = [
         "tenant__first_name",
         "tenant__middle_name",
