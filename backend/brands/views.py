12d75df0daf0579c5aee9efd26aaf20aa4132bb9
zabor <pahapasha11@gmail.com>
2026-05-08 12:21:22 +0700

    fix err is_deleted

--- backend/brands/views.py
+++ backend/brands/views.py
@@ -297,7 +297,7 @@ class BrandViewSet(viewsets.ModelViewSet):
     def nomenclatures(self, request, *args, **kwargs):
         """Номенклатуры прикреплённые к бренду."""
         brand = self.get_object()
-        queryset = brand.nomenclatures.filter(is_deleted=False)
+        queryset = brand.nomenclatures.filter(is_active=False)
 
         paginator = CustomLimitOffsetPagination()
         page = paginator.paginate_queryset(queryset, request)
