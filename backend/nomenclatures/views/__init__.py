882eb0314b064f915f39793d904ca06a01294a7a
Administrator <anankoa@krasrm.com>
2026-05-20 16:43:17 +0700

    откат

--- backend/nomenclatures/views/__init__.py
+++ backend/nomenclatures/views/__init__.py
@@ -1,18 +1,7 @@
 from .nomenclature import NomenclatureViewSet
 from .order import NomenclatureOrderViewSet
 from .place import TypeOfPlaceViewSet
-# from .statistic import NomenclatureStatisticViewSet  ← УДАЛИТЬ (агрегатор)
-
-# ← ИМПОРТИРОВАТЬ КОНКРЕТНЫЕ VIEWSET
-from .statistic import (
-    ADStatisticViewSet,
-    MusicStatisticViewSet,
-    VideoStatisticViewSet,
-    ImageStatisticViewSet,
-    TickerStatisticViewSet,
-    NomenclatureHistoryViewSet,
-)
-
+from .statistic import NomenclatureStatisticViewSet
 from .task import NomenclatureTaskViewSet
 from .photo import NomenclaturePhotoViewSet
 from .tenant import NomenclatureTenantViewSet
@@ -20,33 +9,9 @@ from .tenant import NomenclatureTenantViewSet
 __all__ = [
     'NomenclatureViewSet',
     'NomenclatureOrderViewSet',
-    # 'NomenclatureStatisticViewSet',  ← УДАЛИТЬ
-    'ADStatisticViewSet',
-    'MusicStatisticViewSet',
-    'VideoStatisticViewSet',
-    'ImageStatisticViewSet',
-    'TickerStatisticViewSet',
-    'NomenclatureHistoryViewSet',
+    'NomenclatureStatisticViewSet',
     'NomenclatureTaskViewSet',
     'NomenclaturePhotoViewSet',
     'TypeOfPlaceViewSet',
     'NomenclatureTenantViewSet'
-]
-
-# from .nomenclature import NomenclatureViewSet
-# from .order import NomenclatureOrderViewSet
-# from .place import TypeOfPlaceViewSet
-# from .statistic import NomenclatureStatisticViewSet
-# from .task import NomenclatureTaskViewSet
-# from .photo import NomenclaturePhotoViewSet
-# from .tenant import NomenclatureTenantViewSet
-
-# __all__ = [
-#     'NomenclatureViewSet',
-#     'NomenclatureOrderViewSet',
-#     'NomenclatureStatisticViewSet',
-#     'NomenclatureTaskViewSet',
-#     'NomenclaturePhotoViewSet',
-#     'TypeOfPlaceViewSet',
-#     'NomenclatureTenantViewSet'
 ]
