ba935a41ee10ca5045b18ce7434943be3774e702
Administrator <anankoa@krasrm.com>
2026-05-20 15:35:58 +0700

    откатил измененеия

--- backend/nomenclatures/views/__init__.py
+++ backend/nomenclatures/views/__init__.py
@@ -1,18 +1,7 @@
 from .nomenclature import NomenclatureViewSet
 from .order import NomenclatureOrderViewSet
 from .place import TypeOfPlaceViewSet
-# from .statistic import NomenclatureStatisticViewSet  ← УДАЛИТЬ (старое)
-
-# ← ИМПОРТИРУЕМ ТО, ЧТО НУЖНО (новые классы)
-from ch_statistic.views import (
-    ADStatisticViewSet,
-    MusicStatisticViewSet,
-    VideoStatisticViewSet,
-    ImageStatisticViewSet,
-    TickerStatisticViewSet,
-    NomenclatureHistoryViewSet,
-)
-
+from .statistic import NomenclatureStatisticViewSet
 from .task import NomenclatureTaskViewSet
 from .photo import NomenclaturePhotoViewSet
 from .tenant import NomenclatureTenantViewSet
@@ -20,36 +9,9 @@ from .tenant import NomenclatureTenantViewSet
 __all__ = [
     'NomenclatureViewSet',
     'NomenclatureOrderViewSet',
-    # 'NomenclatureStatisticViewSet',  ← УДАЛИТЬ
-    
-    # ← ДОБАВИТЬ новые
-    'ADStatisticViewSet',
-    'MusicStatisticViewSet', 
-    'VideoStatisticViewSet',
-    'ImageStatisticViewSet',
-    'TickerStatisticViewSet',
-    'NomenclatureHistoryViewSet',
-    
+    'NomenclatureStatisticViewSet',
     'NomenclatureTaskViewSet',
     'NomenclaturePhotoViewSet',
     'TypeOfPlaceViewSet',
     'NomenclatureTenantViewSet'
 ]
-
-# from .nomenclature import NomenclatureViewSet
-# from .order import NomenclatureOrderViewSet
-# from .place import TypeOfPlaceViewSet
-# from .statistic import NomenclatureStatisticViewSet
-# from .task import NomenclatureTaskViewSet
-# from .photo import NomenclaturePhotoViewSet
-# from .tenant import NomenclatureTenantViewSet
-
-# __all__ = [
-#     'NomenclatureViewSet',
-#     'NomenclatureOrderViewSet',
-#     'NomenclatureStatisticViewSet',
-#     'NomenclatureTaskViewSet',
-#     'NomenclaturePhotoViewSet',
-#     'TypeOfPlaceViewSet',
-#     'NomenclatureTenantViewSet'
-# ]
