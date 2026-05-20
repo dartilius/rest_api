da86efca8b59eec483e1ee9a1a60eb0ef9209a31
Administrator <anankoa@krasrm.com>
2026-05-20 15:23:22 +0700

    добавил новые вьюсеты __init__.py

--- backend/nomenclatures/views/__init__.py
+++ backend/nomenclatures/views/__init__.py
@@ -1,7 +1,18 @@
 from .nomenclature import NomenclatureViewSet
 from .order import NomenclatureOrderViewSet
 from .place import TypeOfPlaceViewSet
-from .statistic import NomenclatureStatisticViewSet
+# from .statistic import NomenclatureStatisticViewSet  ← УДАЛИТЬ (старое)
+
+# ← ИМПОРТИРУЕМ ТО, ЧТО НУЖНО (новые классы)
+from ch_statistic.views import (
+    ADStatisticViewSet,
+    MusicStatisticViewSet,
+    VideoStatisticViewSet,
+    ImageStatisticViewSet,
+    TickerStatisticViewSet,
+    NomenclatureHistoryViewSet,
+)
+
 from .task import NomenclatureTaskViewSet
 from .photo import NomenclaturePhotoViewSet
 from .tenant import NomenclatureTenantViewSet
@@ -9,9 +20,36 @@ from .tenant import NomenclatureTenantViewSet
 __all__ = [
     'NomenclatureViewSet',
     'NomenclatureOrderViewSet',
-    'NomenclatureStatisticViewSet',
+    # 'NomenclatureStatisticViewSet',  ← УДАЛИТЬ
+    
+    # ← ДОБАВИТЬ новые
+    'ADStatisticViewSet',
+    'MusicStatisticViewSet', 
+    'VideoStatisticViewSet',
+    'ImageStatisticViewSet',
+    'TickerStatisticViewSet',
+    'NomenclatureHistoryViewSet',
+    
     'NomenclatureTaskViewSet',
     'NomenclaturePhotoViewSet',
     'TypeOfPlaceViewSet',
     'NomenclatureTenantViewSet'
 ]
+
+# from .nomenclature import NomenclatureViewSet
+# from .order import NomenclatureOrderViewSet
+# from .place import TypeOfPlaceViewSet
+# from .statistic import NomenclatureStatisticViewSet
+# from .task import NomenclatureTaskViewSet
+# from .photo import NomenclaturePhotoViewSet
+# from .tenant import NomenclatureTenantViewSet
+
+# __all__ = [
+#     'NomenclatureViewSet',
+#     'NomenclatureOrderViewSet',
+#     'NomenclatureStatisticViewSet',
+#     'NomenclatureTaskViewSet',
+#     'NomenclaturePhotoViewSet',
+#     'TypeOfPlaceViewSet',
+#     'NomenclatureTenantViewSet'
+# ]
