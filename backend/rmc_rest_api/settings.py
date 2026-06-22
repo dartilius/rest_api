fa8ebe3a76f9db38b8d88b175c85aa2de0f84167
Administrator <anankoa@krasrm.com>
2026-06-22 16:58:02 +0700

    fix: параметры settings: LANGUAGE_CODE = 'ru'
    TIME_ZONE = 'Asia/Krasnoyarsk'
    
    USE_I18N = True
    
    USE_TZ = False

--- backend/rmc_rest_api/settings.py
+++ backend/rmc_rest_api/settings.py
@@ -202,9 +202,9 @@ LANGUAGE_CODE = 'ru'
 
 TIME_ZONE = 'Asia/Krasnoyarsk'
 
-#USE_I18N = True
+USE_I18N = True
 
-USE_TZ = True
+USE_TZ = False
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
